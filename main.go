1dab2e82dd1340a3eaa102972be217bb7d6c1b83
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2024-09-21 19:53:46 +0200

    add ttl for cache

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/DmytroBeliasnyk/in_memory_cache/memory"
 )
@@ -9,23 +10,28 @@ import (
 func main() {
 	memCache := memory.GetCache()
 
-	errorsHandler(memCache.Set("login", "password"))
-	errorsHandler(memCache.Set("login2", "password2"))
-	errorsHandler(memCache.Set("login2", "password3"))
+	errorsHandler(memCache.Set("login", "password", time.Hour))
+	errorsHandler(memCache.Set("login", "password", time.Second))
+	errorsHandler(memCache.Set("user1", 1, time.Second))
+	errorsHandler(memCache.Set("count", 9, time.Second*5))
 
 	res, err := memCache.Get("login")
 	errorsHandler(err)
-	fmt.Println(res)
-	fmt.Println()
+	fmt.Printf("%v\n\n", res)
 
 	memCache.Delete("login")
 
-	fmt.Println(memCache)
+	fmt.Println(memCache.String())
+
+	time.Sleep(time.Second * 2)
+	fmt.Println(memCache.String())
+
+	time.Sleep(time.Second * 5)
+	fmt.Println(memCache.String())
 }
 
 func errorsHandler(err error) {
 	if err != nil {
-		fmt.Println(err)
-		fmt.Println()
+		fmt.Printf("%s\n\n", fmt.Errorf("ERROR: %s", err))
 	}
 }
--- memory/cache.go
+++ memory/cache.go
@@ -1,34 +1,75 @@
 package memory
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
 
 var single *cache
 
 type cache struct {
 	memoryCache map[string]interface{}
+	mu          sync.RWMutex
+}
+
+type item struct {
+	key string
+	ttl time.Duration
 }
 
 func GetCache() *cache {
 	if single == nil {
 		single = &cache{
 			memoryCache: make(map[string]interface{}),
+			mu:          sync.RWMutex{},
 		}
 	}
 
 	return single
 }
 
-func (c cache) Set(key string, value interface{}) error {
+func (c *cache) String() (res string) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	res = "IN MEMORY CACHE:\n"
+	for k, v := range c.memoryCache {
+		res += fmt.Sprintf("#%s - %v\n", k, v)
+	}
+
+	return
+}
+
+func (c *cache) Set(key string, value interface{}, ttl time.Duration) error {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+
 	_, exist := single.memoryCache[key]
-	if exist {
-		return errors.New("the key " + key + " already exists")
+
+	switch {
+	case exist:
+		return fmt.Errorf("the key: %s - already exists", key)
+	case ttl <= 0:
+		return fmt.Errorf("invalid time-to-live value: %s", ttl.String())
 	}
 
 	single.memoryCache[key] = value
+
+	items := make(chan item)
+	go c.ttl(items)
+
+	items <- item{key, ttl}
+	close(items)
+
 	return nil
 }
 
-func (c cache) Get(key string) (interface{}, error) {
+func (c *cache) Get(key string) (interface{}, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	res, exist := single.memoryCache[key]
 	if !exist {
 		return 0, errors.New("no mapping for the key " + key)
@@ -37,6 +78,25 @@ func (c cache) Get(key string) (interface{}, error) {
 	return res, nil
 }
 
-func (c cache) Delete(key string) {
+func (c *cache) Delete(key string) {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+
 	delete(single.memoryCache, key)
+}
+
+func (c *cache) ttl(items <-chan item) {
+	timer := func(key string, t time.Duration, wg *sync.WaitGroup) {
+		<-time.After(t)
+		c.Delete(key)
+		wg.Done()
+	}
+
+	wg := &sync.WaitGroup{}
+	for i := range items {
+		wg.Add(1)
+		go timer(i.key, i.ttl, wg)
+	}
+
+	wg.Wait()
 }
